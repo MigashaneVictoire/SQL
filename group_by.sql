26e0c9925a8799ffcfa558cf4cf8642a54cf078e
MigashaneVictoire <Migashanevictoire@gmail.com>
2023-09-15 19:08:16 -0500

    add more group by

--- group_by.sql
+++ group_by.sql
@@ -32,29 +32,48 @@ GROUP BY first_name, last_name
 HAVING first_name LIKE 'E%' AND first_name LIKE '%E'
 	AND last_name LIKE 'E%' AND last_name LIKE '%E';
 -- Write a query to find the unique last names with a 'q' but not 'qu'.
-
-
+SELECT distinct(last_name)
+FROM employees
+WHERE last_name LIKE '%q%' 
+	AND last_name NOT LIKE '%qu%';
+    
+SELECT last_name
+FROM employees
+GROUP BY last_name
+HAVING last_name LIKE '%q%' 
+	AND last_name NOT LIKE '%qu%';
 -- Add a COUNT() to your results for exercise 5 to find the number of employees with the same last name.
-
+SELECT last_name, count(last_name)
+FROM employees
+GROUP BY last_name
+HAVING last_name LIKE '%q%' 
+	AND last_name NOT LIKE '%qu%';
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees with those names for each gender.
+SELECT gender, count(first_name)
+FROM (SELECT first_name, gender
+		FROM employees
+		WHERE first_name IN ('Irena', 'Vidya', 'Maya')) AS names
+GROUP BY gender;
 
--- Using your query that generates a username for all employees, generate a count of employees with each unique username.
+SELECT gender, count(first_name)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
 
--- From your previous query, are there any duplicate usernames? What is the highest number of times a username shows up? Bonus: How many duplicate usernames are there?
-
--- Determine the historic average salary for each employee. When you hear, read, or think "for each" with regard to SQL, you'll probably be grouping by that exact column.
-
--- Using the dept_emp table, count how many current employees work in each department. The query result should show 9 rows, one for each department and the employee count.
-
+-- Determine the historic average salary for each employee.
+SELECT emp_no, avg(salary)
+FROM salaries
+GROUP BY emp_no;
+-- Using the dept_emp table, count how many current employees work in each department.
+SELECT dept_no, count(emp_no)
+FROM dept_emp
+GROUP BY dept_no;
 -- Determine how many different salaries each employee has had. This includes both historic and current.
-
+SELECT emp_no, count(salary)
+FROM salaries
+GROUP BY emp_no;
 -- Find the maximum salary for each employee.
-
--- Find the minimum salary for each employee.
-
--- Find the standard deviation of salaries for each employee.
-
--- Find the max salary for each employee where that max salary is greater than $150,000.
-
--- Find the average salary for each employee where that average salary is between $80k and $90k.
+SELECT emp_no, max(salary)
+FROM salaries
+GROUP BY emp_no;
 
